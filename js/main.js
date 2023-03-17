9a91a462dae10cd9a08943287930f8c89ca2cd22
shebl <shebblloll@gmail.com>
2023-03-17 13:11:42 +0200

    count to data set feature( stats section )

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ let btn = document.querySelector(".scroll-top");
 let megaBtn = document.querySelector(".mega-btn");
 let width = document.querySelector(".scroll-par .width");
 let skillsSection = document.querySelector(".skills");
+let statsSection = document.querySelector(".stats");
+let statBoxes = document.querySelectorAll(".stats .box h1");
 let spans = document.querySelectorAll(".skills span");
 let boxes = document.querySelectorAll(".video .box");
 let images = document.querySelectorAll(".video .image img");
+let flag = false;
+
+//////////////////////////images taps in video section /////////////////////////////
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     // console.log(box.classList[0]);
@@ -19,7 +24,9 @@ boxes.forEach((box) => {
     box.classList.add("active");
   });
 });
-console.log(boxes);
+// console.log(boxes);
+//////////////////////////end images taps in video section /////////////////////////////
+
 window.onscroll = () => {
   ///////////////// go to up btn feature //////////////////////////
   if (
@@ -30,6 +37,25 @@ window.onscroll = () => {
     btn.style.right = "-137px";
   }
   ///////////////// end go to up btn feature//////////////////////////
+
+  ///////////////// start ( stats section ) count to data set feature//////////////////////////
+  if (window.scrollY > statsSection.offsetTop - 300) {
+    if (!flag) {
+      statBoxes.forEach((statBox) => {
+        let counter = setInterval(() => {
+          statBox.innerHTML++;
+          if (statBox.innerHTML == statBox.parentElement.dataset.number) {
+            clearInterval(counter);
+            // console.log("done");
+          }
+        }, 3000 / statBox.parentElement.dataset.number);
+      });
+      flag = true;
+    }
+  } else {
+  }
+  ///////////////// end ( stats section ) count to data set feature//////////////////////////
+
   ///////////////// start full progress bar animation//////////////////////////
   if (window.scrollY > skillsSection.offsetTop - 300) {
     // console.log(skillsSection.offsetTop);
@@ -42,6 +68,7 @@ window.onscroll = () => {
     });
   }
   ///////////////// end full progress bar animation//////////////////////////
+
   ///////////////// scroll bar progress feature//////////////////////////
   let scrollTop = document.documentElement.scrollTop;
   let scroll =
@@ -67,7 +94,7 @@ btn.addEventListener("click", () => {
 });
 /////////////////////////////// end button go to up page feature /////////////////////////////////
 
-/////////////////////////////// add Event feature ///////////////////////////////////////////
+/////////////////////////////// add Event feature ( count down timer  )///////////////////////////////////////////
 let dateOfEvent = new Date("2024-12-26").getTime();
 
 let counter = setInterval(() => {
@@ -84,5 +111,4 @@ let counter = setInterval(() => {
   document.querySelector(".events .second h1").innerHTML = Math.floor(secs);
   // console.log(hours);
 }, 1000);
-
-/////////////////////////////// end Event feature ///////////////////////////////////////////
+/////////////////////////////// end Event feature ( count down timer  )///////////////////////////////////////////
